4d9009613d2328c4c04fb36ee0702ce9b34886f7
Daniel Lopez <d.lopez.1740@gmail.com>
2021-04-27 13:38:43 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,29 @@ import "reflect-metadata";
 import * as express from "express";
 import * as cors from 'cors';
 import * as helmet from 'helmet';
-import { createConnection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 import routes from './routes';
+import { UserEntity } from './entities';
 
 const PORT = process.env.APP_PORT || 3000;
+
+async function registerUser({ manager }: Connection): Promise<void> {
+  const users = await manager.find(UserEntity);
+  if (users.length === 0) {
+    const user = new UserEntity();
+    user.username = 'daniel1';
+    user.password = '12345678';
+    user.role = 'admin';
+    await manager.save(user);
+  }
+}
 
 async function bootstrap() {
   let retries = 5;
   while (retries) {
     try {
-      await createConnection();
+      const connection = await createConnection();
+      registerUser(connection);
       break;
     } catch (err) {
       retries -= 1;
